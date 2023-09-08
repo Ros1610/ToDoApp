37874e6ff2d8f579c748a8c35f6dc1b7f6c15c40
Ros <ros.pol16@gmail.com>
2023-09-08 11:54:20 +0200

    Implement Add button, close window

--- gui.py
+++ gui.py
@@ -2,11 +2,25 @@ import PySimpleGUI as sg
 import functions
 
 label = sg.Text("Type in a to-do")
-input_box = sg.InputText(tooltip="Enter a to-do")
+input_box = sg.InputText(tooltip="Enter a to-do",
+                         key="todo")
 add_button = sg.Button("Add")
 
+window = sg.Window('My To-Do App',
+                   layout=[[label], [input_box, add_button]],
+                   font=('Helvetica', 11))
 
-window = sg.Window('My To-Do App', layout=[[label],[input_box, add_button]])
-window.read()
+todos = functions.read_file()
+
+while True:
+    event, values = window.read()
+    match event:
+        case "Add":
+            todos.append(values["todo"].strip().capitalize() + "\n")
+
+        case sg.WIN_CLOSED:
+            functions.write_file(todos)
+            break
+
+
 window.close()
-
